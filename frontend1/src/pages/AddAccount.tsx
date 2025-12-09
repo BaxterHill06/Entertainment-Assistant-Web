8b94762f3d4e10fc9c702d4d9986af58765732d0
BAXTER HILL <s4171523@student.rmit.edu.au>
2025-12-09 23:38:52 +1100

    reworked (issue with displaying page)

---
+++ frontend1/src/pages/AddAccount.tsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
+function AddAccount() {
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <h1>add</h1>
+
+        </div>
+    );
+}
+
+export default AddAccount;
